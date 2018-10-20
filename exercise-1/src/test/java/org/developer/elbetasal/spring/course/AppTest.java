ba4fdc66306a6104c2e31607a9ddd2080a51a284
Esau Betancourt <esau.betancourt@gmail.com>
2018-10-19 20:41:13 -0500

    Initial Commit for the exercises

---
+++ exercise-1/src/test/java/org/developer/elbetasal/spring/course/AppTest.java
@@ -0,0 +1,20 @@
+package org.developer.elbetasal.spring.course;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
