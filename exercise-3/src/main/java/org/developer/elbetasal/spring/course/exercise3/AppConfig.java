99231aee8e27b4f4f664eb355968885f24c5a6ba
Esau Betancourt <esau.betancourt@gmail.com>
2018-11-16 19:24:31 -0600

    Agregando la configuracion de spring

---
+++ exercise-3/src/main/java/org/developer/elbetasal/spring/course/exercise3/AppConfig.java
@@ -0,0 +1,61 @@
+package org.developer.elbetasal.spring.course.exercise3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@ComponentScan
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:jdbc.properties")
+public class AppConfig {
+
+	@Value("${jdbc.app.password}")
+	private String sqluserpassword ;
+
+	@Value("${jdbc.app.username}")
+	private String username;
+
+	@Value("${jdbc.app.driver}")
+	private String driver;
+
+	@Value("${jdbc.app.url}")
+	private String url;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName(driver);
+		driverManagerDataSource.setUrl(url);
+		driverManagerDataSource.setUsername(username);
+		driverManagerDataSource.setPassword(sqluserpassword);
+		return driverManagerDataSource;
+
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertPlaceHolderConfiguer() {
+
+		return new PropertySourcesPlaceholderConfigurer();
+
+	}
+
+
+
+}
--- exercise-3/src/main/java/org/developer/elbetasal/spring/course/exercise3/StudentRepositoryImpl.java
+++ exercise-3/src/main/java/org/developer/elbetasal/spring/course/exercise3/StudentRepositoryImpl.java
@@ -1,8 +1,19 @@
 package org.developer.elbetasal.spring.course.exercise3;
 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
 import java.util.List;
 
+@Repository
 public class StudentRepositoryImpl implements StudentRepository {
+
+	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+	public StudentRepositoryImpl(DataSource dataSource) {
+		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+	}
 
 	@Override
 	public Student create(String name, Integer age) {
