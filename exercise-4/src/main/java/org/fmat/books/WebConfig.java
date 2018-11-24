b61869a4e8da4531347fcb40d3c100cdcdcab040
Esau Betancourt <esau.betancourt@gmail.com>
2018-11-24 10:06:09 -0600

    Integracion con Thymeleaf

--- exercise-4/src/main/java/org/fmat/books/WebConfig.java
+++ exercise-4/src/main/java/org/fmat/books/WebConfig.java
@@ -1,19 +1,30 @@
 package org.fmat.books;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan
 public class WebConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private ServletContext ctx;
 
 	@Override
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
@@ -28,13 +39,30 @@ public class WebConfig implements WebMvcConfigurer {
 	}
 
 	@Bean
-	public InternalResourceViewResolver resolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setViewClass(JstlView.class);
-		resolver.setPrefix("/WEB-INF/");
-		resolver.setSuffix(".jsp");
-		resolver.setOrder(0);
-		return resolver;
+	public ViewResolver thymeleafViewResolver() {
+		final ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setOrder(1);
+		return viewResolver;
+	}
+
+	@Bean
+	@Description("Thymeleaf template engine with Spring integration")
+	public SpringTemplateEngine templateEngine() {
+		final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
+	}
+
+	@Bean
+	@Description("Thymeleaf template resolver serving HTML 5")
+	public ServletContextTemplateResolver templateResolver() {
+		final ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(ctx);
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setName("templates");
+		templateResolver.setTemplateMode("HTML5");
+		return templateResolver;
 	}
 
 
